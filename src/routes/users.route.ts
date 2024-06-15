a3eb282841b873beb5a10e3972a798284ecaf12b
Hebib Qurbanov <qurbanovhebib554@gmail.com>
2024-06-15 23:59:20 +0400

    add /me, add /:username, update schema, add getuserprofile, fix other services

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -5,12 +5,11 @@ import httpStatus from "http-status";
 import { getSignedCookie } from "hono/cookie";
 import * as sessionService from "../services/session.service";
 import { ApiError } from "../utils/ApiError";
+import { getUserProfile } from "../services/user.service";
 
 const userRoute = new Hono<Environment>();
 
 export default userRoute;
-
-// /users/me , users/{username} , users?q= ,
 
 userRoute.get("/me", async (c) => {
   const sessionID = await getSignedCookie(c, c.env.HMACsecret, "SID");
@@ -26,4 +25,30 @@ userRoute.get("/me", async (c) => {
   if (!session.values.email_verified) {
     throw new ApiError(httpStatus.UNAUTHORIZED, "User is not verified.");
   }
+  const userProfile = await getUserProfile(
+    session.values.user_id,
+    c.env.DATABASE_URL
+  );
+
+  return c.json(userProfile, httpStatus.CREATED as StatusCode);
 });
+
+userRoute.get("/:username", async (c) => {
+  const sessionID = await getSignedCookie(c, c.env.HMACsecret, "SID");
+  let session;
+  if (sessionID != null) {
+    session = await sessionService.validSession(sessionID.toString(), {
+      Bindings: c.env,
+    });
+  }
+
+  let username = c.req.param("username");
+  const userProfile = await getUserProfile(
+    session?.values.user_id,
+    c.env.DATABASE_URL,
+    username
+  );
+  return c.json(userProfile, httpStatus.CREATED as StatusCode);
+});
+
+//add user search
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,11 @@
 import { createDatabaseConnection } from "../db/connection";
-import { users } from "../db/schema";
-import { eq } from "drizzle-orm";
+import { books, users, shelves, book_genres, genres } from "../db/schema";
+import {
+  PgSelectQueryBuilder,
+  QueryBuilder,
+  PgSelectQueryBuilderBase,
+} from "drizzle-orm/pg-core";
+import { eq, and } from "drizzle-orm";
 import { nanoid } from "nanoid";
 import httpStatus from "http-status";
 import { ApiError } from "../utils/ApiError";
@@ -39,7 +44,6 @@ export const loginUser = async (email: string, databaseConfig: string) => {
   let result;
   const db = await createDatabaseConnection(databaseConfig);
   try {
-    // result = await db.select().from(users).where(eq(users.email, email));
     result = await db
       .update(users)
       .set({ last_sign_in_at: new Date() })
@@ -70,4 +74,147 @@ export const getUserProfile = async (
   id: string,
   databaseConfig: string,
   username: string = ""
-) => {};
+) => {
+  const db = await createDatabaseConnection(databaseConfig);
+  try {
+    const result = await db.transaction(async (trx) => {
+      let user: any;
+      if (username !== "") {
+        user = await trx
+          .select()
+          .from(users)
+          .where(eq(users.username, username));
+      } else {
+        user = await trx.select().from(users).where(eq(users.id, id));
+      }
+
+      if (!user || user.length === 0) {
+        throw new ApiError(httpStatus.NOT_FOUND, "User not found");
+      }
+
+      user = user[0];
+      const isCurrentUser = user.id === id;
+
+      if (!isCurrentUser && user.private) {
+        return {
+          user: {
+            id: user.id,
+            username: user.username,
+            bio: user.bio,
+            avatar: user.avatar,
+            banner: user.banner,
+            private: user.isPrivate,
+            created_at: user.createdAt,
+          },
+          books: {
+            favorites: [],
+            reading: [],
+            read: [],
+          },
+          shelves: [],
+        };
+      }
+      const qb = new QueryBuilder();
+      let userBooks = qb
+        .select()
+        .from(books)
+        .where(eq(books.user_id, user.id))
+        .$dynamic();
+
+      let userShelves = await trx
+        .select()
+        .from(shelves)
+        .where(eq(shelves.user_id, user.id));
+
+      function ByStatus<T extends PgSelectQueryBuilder>(
+        qb: T,
+        status: string,
+        privateBooks: boolean = false
+      ) {
+        if (status === "favorites") {
+          if (privateBooks) {
+            return qb.where(eq(books.favorite, true)).limit(5);
+          } else {
+            return qb
+              .where(and(eq(books.favorite, true), eq(books.private, false)))
+              .limit(5);
+          }
+        }
+        if (status === "reading") {
+          if (privateBooks) {
+            return qb.where(eq(books.finished, false)).limit(5);
+          } else {
+            return qb
+              .where(and(eq(books.finished, false), eq(books.private, false)))
+              .limit(5);
+          }
+        }
+        if (status === "read") {
+          if (privateBooks) {
+            return qb.where(eq(books.finished, true)).limit(5);
+          } else {
+            return qb
+              .where(and(eq(books.finished, true), eq(books.private, false)))
+              .limit(5);
+          }
+        }
+      }
+
+      if (!isCurrentUser && !user.isPrivate) {
+        ByStatus(userBooks, "reading", false);
+        let reading = (await trx.execute(userBooks)).rows;
+        ByStatus(userBooks, "read", false);
+        let read = (await trx.execute(userBooks)).rows;
+        ByStatus(userBooks, "favorites", false);
+        let favorite = (await trx.execute(userBooks)).rows;
+
+        userShelves = userShelves.filter((shelf: any) => !shelf.isPrivate);
+        return {
+          user: {
+            id: user.id,
+            username: user.username,
+            bio: user.bio,
+            avatar: user.avatar,
+            banner: user.banner,
+            private: user.isPrivate,
+            created_at: user.createdAt,
+          },
+          books: {
+            favorites: favorite,
+            reading: reading,
+            read: read,
+          },
+          shelves: userShelves,
+        };
+      }
+
+      if (isCurrentUser) {
+        ByStatus(userBooks, "reading", true);
+        let reading = (await trx.execute(userBooks)).rows;
+        ByStatus(userBooks, "read", true);
+        let read = (await trx.execute(userBooks)).rows;
+        ByStatus(userBooks, "favorites", true);
+        let favorite = (await trx.execute(userBooks)).rows;
+        return {
+          user: user,
+          books: {
+            favorites: favorite,
+            reading: reading,
+            read: read,
+          },
+          shelves: userShelves,
+        };
+      }
+    });
+
+    return result;
+  } catch (error) {
+    if (
+      error instanceof ApiError &&
+      error.statusCode === httpStatus.NOT_FOUND
+    ) {
+      throw error;
+    }
+    throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR, "Failed to get user");
+  }
+};
